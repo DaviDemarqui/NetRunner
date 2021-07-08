11f29f2c3a77cbead012c70846ded9f5b33dad14
DaviDemarqui <79232502+DaviDemarqui@users.noreply.github.com>
2021-07-08 17:23:33 -0300

    Update Nave.cs

--- Nave.cs
+++ Nave.cs
@@ -6,7 +6,7 @@ public class Nave : MonoBehaviour
     public Transform[] target;
     public float speed;
     private int current;
-    // Use this for initialization    
+    // Use this for initialization.    
     void Start() { }
     // Update is called once per frame.    
     void Update()
